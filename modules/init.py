dec1a7469ae6b1ffce356665e3023474d085cdd4
vortex22 <226747722+fawwaznasuha12-afk@users.noreply.github.com>
2026-07-19 20:50:09 +0700

    Create init.py

---
+++ modules/init.py
@@ -0,0 +1,17 @@
+from modules.email import EmailOSINT
+from modules.username import UsernameOSINT
+from modules.domain import DomainOSINT
+from modules.social import SocialOSINT
+from modules.phone import PhoneOSINT
+from modules.image import ImageOSINT
+from modules.crypto import CryptoOSINT
+
+__all__ = [
+    'EmailOSINT',
+    'UsernameOSINT',
+    'DomainOSINT',
+    'SocialOSINT',
+    'PhoneOSINT',
+    'ImageOSINT',
+    'CryptoOSINT'
+]
