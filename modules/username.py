e31ccf45284e8be0b4a227b19e579b59cdb580ea
vortex22 <226747722+fawwaznasuha12-afk@users.noreply.github.com>
2026-07-20 16:59:13 +0700

    Update username.py

--- modules/username.py
+++ modules/username.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 from typing import Dict, List
+from datetime import datetime
 from modules.base import BaseModule
 
 class UsernameOSINT(BaseModule):
@@ -9,25 +10,31 @@ class UsernameOSINT(BaseModule):
         
     async def scan(self, username: str) -> Dict:
         results = {
-            'username': username,
-            'found': [],
-            'not_found': [],
-            'total': 0,
-            'platforms': {
-                'social': [],
-                'video': [],
-                'forum': [],
-                'blog': [],
-                'audio': [],
-                'photo': [],
-                'gaming': [],
-                'chat': [],
-                'coding': [],
-                'dating': []
-            }
+            'target': username,
+            'module': 'username',
+            'status': 'success',
+            'data': {
+                'username': username,
+                'found': [],
+                'not_found': [],
+                'total': 0,
+                'platforms': {
+                    'social': [],
+                    'video': [],
+                    'forum': [],
+                    'blog': [],
+                    'audio': [],
+                    'photo': [],
+                    'gaming': [],
+                    'chat': [],
+                    'coding': [],
+                    'dating': []
+                }
+            },
+            'timestamp': datetime.utcnow().isoformat()
         }
         
-        sites = self._get_social_platforms()
+        sites = self._get_platforms()
         tasks = []
         for site in sites:
             url = site['url'].format(username=username)
@@ -35,21 +42,22 @@ class UsernameOSINT(BaseModule):
             
         responses = await asyncio.gather(*tasks)
         
+        data = results['data']
         for name, url, exists, platform_type in responses:
             if exists:
                 entry = {'name': name, 'url': url, 'type': platform_type}
-                results['found'].append(entry)
-                if platform_type in results['platforms']:
-                    results['platforms'][platform_type].append(name)
+                data['found'].append(entry)
+                if platform_type in data['platforms']:
+                    data['platforms'][platform_type].append(name)
             else:
-                results['not_found'].append(name)
+                data['not_found'].append(name)
                 
-        results['total'] = len(results['found'])
+        data['total'] = len(data['found'])
         return results
         
-    def _get_social_platforms(self):
+    def _get_platforms(self):
         return [
-            # Social Media Utama
+            # Social Media
             {'name': 'Instagram', 'url': 'https://instagram.com/{username}', 'type': 'social'},
             {'name': 'Twitter', 'url': 'https://twitter.com/{username}', 'type': 'social'},
             {'name': 'Facebook', 'url': 'https://facebook.com/{username}', 'type': 'social'},
@@ -69,33 +77,28 @@ class UsernameOSINT(BaseModule):
             {'name': 'Twitch', 'url': 'https://twitch.tv/{username}', 'type': 'video'},
             {'name': 'Vimeo', 'url': 'https://vimeo.com/{username}', 'type': 'video'},
             {'name': 'DailyMotion', 'url': 'https://dailymotion.com/{username}', 'type': 'video'},
-            {'name': 'Vine', 'url': 'https://vine.co/{username}', 'type': 'video'},
             
-            # Forum & Discussion
+            # Forum
             {'name': 'Reddit', 'url': 'https://reddit.com/user/{username}', 'type': 'forum'},
             {'name': 'Quora', 'url': 'https://quora.com/profile/{username}', 'type': 'forum'},
             {'name': 'StackOverflow', 'url': 'https://stackoverflow.com/users/{username}', 'type': 'forum'},
             {'name': 'HackerNews', 'url': 'https://news.ycombinator.com/user?id={username}', 'type': 'forum'},
             {'name': 'Pastebin', 'url': 'https://pastebin.com/u/{username}', 'type': 'forum'},
-            {'name': 'XDA', 'url': 'https://xdaforums.com/member/{username}', 'type': 'forum'},
             
             # Blog
             {'name': 'Medium', 'url': 'https://medium.com/@{username}', 'type': 'blog'},
             {'name': 'Substack', 'url': 'https://substack.com/@{username}', 'type': 'blog'},
             {'name': 'WordPress', 'url': 'https://wordpress.com/{username}', 'type': 'blog'},
             {'name': 'Blogger', 'url': 'https://blogger.com/{username}', 'type': 'blog'},
-            {'name': 'Ghost', 'url': 'https://ghost.org/{username}', 'type': 'blog'},
             {'name': 'Dev.to', 'url': 'https://dev.to/{username}', 'type': 'blog'},
-            {'name': 'Hashnode', 'url': 'https://hashnode.com/@{username}', 'type': 'blog'},
             
             # Audio
             {'name': 'Spotify', 'url': 'https://open.spotify.com/user/{username}', 'type': 'audio'},
             {'name': 'SoundCloud', 'url': 'https://soundcloud.com/{username}', 'type': 'audio'},
             {'name': 'Mixcloud', 'url': 'https://mixcloud.com/{username}', 'type': 'audio'},
-            {'name': 'Audiomack', 'url': 'https://audiomack.com/{username}', 'type': 'audio'},
             {'name': 'Bandcamp', 'url': 'https://bandcamp.com/{username}', 'type': 'audio'},
             
-            # Photo & Art
+            # Photo
             {'name': 'Flickr', 'url': 'https://flickr.com/people/{username}', 'type': 'photo'},
             {'name': '500px', 'url': 'https://500px.com/{username}', 'type': 'photo'},
             {'name': 'Unsplash', 'url': 'https://unsplash.com/@{username}', 'type': 'photo'},
@@ -110,22 +113,15 @@ class UsernameOSINT(BaseModule):
             {'name': 'Xbox', 'url': 'https://xbox.com/{username}', 'type': 'gaming'},
             {'name': 'Nintendo', 'url': 'https://nintendo.com/{username}', 'type': 'gaming'},
             {'name': 'EpicGames', 'url': 'https://epicgames.com/{username}', 'type': 'gaming'},
-            {'name': 'RiotGames', 'url': 'https://riotgames.com/{username}', 'type': 'gaming'},
-            {'name': 'BattleNet', 'url': 'https://battlenet.com/{username}', 'type': 'gaming'},
-            {'name': 'Origin', 'url': 'https://origin.com/{username}', 'type': 'gaming'},
             
-            # Chat & Messaging
+            # Chat
             {'name': 'Telegram', 'url': 'https://t.me/{username}', 'type': 'chat'},
             {'name': 'Discord', 'url': 'https://discord.com/users/{username}', 'type': 'chat'},
             {'name': 'WhatsApp', 'url': 'https://wa.me/{username}', 'type': 'chat'},
             {'name': 'Line', 'url': 'https://line.me/{username}', 'type': 'chat'},
             {'name': 'KakaoTalk', 'url': 'https://kakao.com/{username}', 'type': 'chat'},
-            {'name': 'WeChat', 'url': 'https://wechat.com/{username}', 'type': 'chat'},
-            {'name': 'Signal', 'url': 'https://signal.org/{username}', 'type': 'chat'},
-            {'name': 'Kik', 'url': 'https://kik.com/{username}', 'type': 'chat'},
-            {'name': 'Viber', 'url': 'https://viber.com/{username}', 'type': 'chat'},
             
-            # Coding & Developer
+            # Coding
             {'name': 'GitHub', 'url': 'https://github.com/{username}', 'type': 'coding'},
             {'name': 'GitLab', 'url': 'https://gitlab.com/{username}', 'type': 'coding'},
             {'name': 'Bitbucket', 'url': 'https://bitbucket.org/{username}', 'type': 'coding'},
@@ -135,22 +131,10 @@ class UsernameOSINT(BaseModule):
             {'name': 'HackerRank', 'url': 'https://hackerrank.com/{username}', 'type': 'coding'},
             {'name': 'Codeforces', 'url': 'https://codeforces.com/profile/{username}', 'type': 'coding'},
             
-            # Professional
-            {'name': 'Keybase', 'url': 'https://keybase.io/{username}', 'type': 'coding'},
+            # Security
             {'name': 'HackTheBox', 'url': 'https://hackthebox.eu/home/users/profile/{username}', 'type': 'coding'},
             {'name': 'TryHackMe', 'url': 'https://tryhackme.com/p/{username}', 'type': 'coding'},
-            
-            # Email
-            {'name': 'ProtonMail', 'url': 'https://protonmail.com/{username}', 'type': 'email'},
-            {'name': 'GMX', 'url': 'https://gmx.com/{username}', 'type': 'email'},
-            {'name': 'Yandex', 'url': 'https://yandex.ru/{username}', 'type': 'email'},
-            {'name': 'MailRu', 'url': 'https://mail.ru/{username}', 'type': 'email'},
-            
-            # Cloud
-            {'name': 'GoogleDrive', 'url': 'https://drive.google.com/{username}', 'type': 'storage'},
-            {'name': 'Dropbox', 'url': 'https://dropbox.com/{username}', 'type': 'storage'},
-            {'name': 'OneDrive', 'url': 'https://onedrive.com/{username}', 'type': 'storage'},
-            {'name': 'Mega', 'url': 'https://mega.nz/{username}', 'type': 'storage'}
+            {'name': 'Keybase', 'url': 'https://keybase.io/{username}', 'type': 'coding'}
         ]
         
     async def _check_site(self, name, url, platform_type):
